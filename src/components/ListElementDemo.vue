f1c0e1c0a72c9f80880b10a86d7a4180cec4ded7
Emmy Sortland <emmy@jobbnorge.no>
2020-05-14 18:07:24 +0200

    fix:Forgot some files

---
+++ src/components/ListElementDemo.vue
@@ -0,0 +1,100 @@
+<template>
+  <div class="wrapper">
+  <JnCard
+      title="Annonse"
+    >
+      <template #content>
+        <ul>
+      <JnListElement
+        isExternal= true
+        title="Jobbnorge.no"
+        secondaryText="Publisert - 10 dager igjen"
+        icon="file-alt"
+        url="https://jobbnorge.no"
+        borderBetweenElements= true
+        navigable= true
+      >
+      </JnListElement> </ul>
+      </template>
+      <!-- <JnListElement
+        isExternal= true
+        title="Jobbnorge.no"
+        secondaryText="Utgått - 2 dager siden"
+        icon="file-alt"
+        url="https://jobbnorge.no"
+        borderBetweenElements= true
+        navigable= true
+      >
+      </JnListElement>
+      <JnListElement
+        isExternal= true
+        title="Jobbnorge.no"
+        secondaryText="Publisert - 10 dager igjen"
+        icon="file-alt"
+        url="https://jobbnorge.no"
+        borderBetweenElements= true
+        navigable= true
+      >
+      </JnListElement>
+    </template>
+    <template #button>
+      <JnTextButton>Alle annonser</JnTextButton> -->
+    <!-- </template>
+    </JnCard>
+    <JnCard
+      title="Dokumenter"
+    >
+      <template #content>
+      <JnListElement
+        isExternal= false
+        title="Innstillingsdokument (standard)"
+        icon="user-secret"
+        url="https://jobbnorge.no"
+        navigable= true
+      >
+      </JnListElement>
+      <JnListElement
+        isExternal= false
+        title="Tilbudsbrev"
+        icon="user-secret"
+        url="https://jobbnorge.no"
+        navigable= true
+      >
+      </JnListElement>
+      <JnListElement
+        isExternal= false
+        title="Avslagsbrev"
+        icon="user-secret"
+        url="https://jobbnorge.no"
+        navigable= true
+      >
+      </JnListElement>
+    </template> -->
+    <template #button>
+      <JnTextButton>Gå til dokumenter</JnTextButton>
+    </template>
+    </JnCard>
+  </div>
+</template>
+
+<script>
+import JnListElement from "./../ui_components/lists/JnListElement";
+import JnCard from "./../ui_components/cards/JnCard"; 
+import JnTextButton from './../ui_components/buttons/JnTextButton';
+
+export default {
+    name: "demoList",
+    components:{
+        JnListElement,
+        JnCard,
+        JnTextButton
+    }
+}
+</script>
+<style scoped>
+  .wrapper {
+    display: grid; 
+    grid-template-columns: 1fr 1fr 1fr;  
+    gap: 1rem; 
+  }
+</style>
---
+++ src/ui_components/lists/JnListElement.vue
@@ -0,0 +1,87 @@
+<template>
+    <li
+        class="grid-wrapper"
+        v-bind:class="{ borderBetween: borderBetweenElements, grow: navigable }"
+        @click="navigable ? navigate() : null "
+        >
+        <i class="icon"><fa-icon :icon="['fal', icon]" size="lg"/></i>
+        <div>
+            <p v-if="secondaryText" class="secondary-text">{{secondaryText}}</p>
+            <p class="title">{{title}}</p>
+        </div>
+        <div v-if="navigable">
+            <i v-if="isExternal" class="center"><fa-icon :icon="['fal','external-link']" class="icon" size="lg"/></i>
+            <i v-else class="center"><fa-icon :icon="['fal', 'chevron-right']" class="icon" size="lg"/></i>
+        </div>
+    </li>
+</template>
+
+<script>
+
+export default {
+    name: "JnList",
+    props: {
+        icon: String,
+        title: String,
+        secondaryText: String,
+        isExternal: Boolean,
+        url: String,
+        borderBetweenElements: Boolean,
+        navigable: Boolean,
+        listData: Array
+    },
+    methods: {
+        navigate(){
+            window.location.href = this.url
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+    .grid-wrapper{
+        display: grid;
+        grid-template-columns: 1fr 6fr 1fr;
+        align-items: center;
+        color: #44303C;
+        padding: 5px 0 5px 0;
+        column-gap: 0.5rem;
+    }
+    .icon{
+        margin-left: 0.5rem;
+    }
+    .title{
+        margin-bottom: 0.5rem;
+    }
+    .secondary-text{
+        color: #127DAC;
+        font-size: 0.75rem;
+        margin: 0.5rem 0 0.25rem 0;
+    }
+    .borderBetween{
+        border-bottom: 1px solid rgba(68, 48, 60, 0.3);
+    }
+    .grow{
+        transition: all .2s ease-in;
+    }
+    .grow:hover{
+        transform: scale(1.03);
+    }
+    a:link{
+        color:#44303C;
+        text-decoration: none;
+    }
+    a:visited{
+        color:#44303C;
+    }
+    a:hover{
+        color:#44303C;
+    }
+    a:active{
+        color:#44303C;
+    }
+    .center{
+        justify-self: center;
+    }
+</style>
