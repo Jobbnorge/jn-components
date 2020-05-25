fb96f5c29e728f32811756a0de25bc901dcb1b12
viljeb-jn <vilje@jobbnorge.no>
2020-05-25 11:21:14 +0200

    cleaned up

--- src/components/AssessmentExample.vue
+++ src/components/AssessmentExample.vue
@@ -1,23 +1,19 @@
 <template>
     <div>
-        <Rating :size="20" /> 
-        
         <div class="container">
-            <JnPickDateTime :action="tellTime" colorTheme="" />
+            <JnPickDateTime :action="tellTime" colorTheme="green" />
         </div>
         <JnCounter :startAt="20" :max="20" :min="1" message="Du har nådd enden på lista"/>
     </div>
     
 </template>
 <script>
-    import Rating from './../ui_components/assessment/Rating';
     import JnPickDateTime from './../ui_components/datetime/JnPickDateTime';
     import JnCounter from './../ui_components/assessment/JnCounter';
 
     export default {
         name: "AsessmentExample",
         components: {
-            Rating,
             JnPickDateTime,
             JnCounter
         },
