2e11097d9dbb2fe007bbe7ec5cfcc7c8cffaf046
Emmy Sortland <emmy@jobbnorge.no>
2020-06-05 10:21:19 +0200

    fix:switch directory for styleguide

--- styleguide.config.js
+++ styleguide.config.js
@@ -1,14 +1,15 @@
 const path = require('path')
 module.exports = {
- sections: [
-    {
-        name: 'ui-components',
-        components:  'src/ui_components/**/*.vue'
-    }
+    sections: [
+        {
+            name: 'ui-components',
+            components: 'src/ui_components/**/*.vue'
+        }
     ],
     require: [
-        
+
         path.join(__dirname, 'assets/css/bootstrap_limitless.css'),
         path.join(__dirname, 'assets/css/Jobbadmin_responsive.css')
-    ]
+    ],
+    styleguideDir: "public/styleguide"
 }
