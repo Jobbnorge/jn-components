083a7cdfa3741e8a0fef9ad2fa176a05606806a5
emmy <emmy@jobbnorge.no>
2020-09-07 09:41:07 +0200

    fix: La til enda 1 eksempel

---
+++ src/components/JnTextButtonExample.vue
@@ -0,0 +1,17 @@
+<template>
+<div>
+    <JnTextButton icon="download" id="1" default="tekst" >
+    <template #default>
+        Trykk på meg
+    </template>
+    </JnTextButton>
+</div>
+</template>
+
+<script>
+import JnTextButton from "./../ui_components/buttons/JnTextButton"
+
+export default {
+    name: "JnTextButtonExample"
+}
+</script>
