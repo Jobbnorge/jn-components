beaa2755b48c7e742466a2543cb44750e5b5998a
Håvard Heim <havardh@jobbnorge.no>
2020-09-11 10:57:25 +0200

    FIX: More padding on JnInfoCard, moved 'x' to head

--- src/ui_components/cards/JnInfoCard.vue
+++ src/ui_components/cards/JnInfoCard.vue
@@ -1,12 +1,12 @@
 <template>
   <article class="info-wrapper" v-if="showInfoCard">
-    <button class="btn end muted" type="button" v-if="removable" @click="hideInfoCard">
-      <fa-icon :icon="['fas', 'times']" />
-    </button>
     <div>
       <fa-icon :icon="['fal', icon]" color="#127dac" size="2x" />
       <header>
         <h1 class="info-header">{{title}}</h1>
+        <a class="end muted" href="#" role="button" v-if="removable" @click="hideInfoCard">
+          <fa-icon :icon="['fas', 'times']" />
+        </a>
       </header>
       <section>
         <p>{{body}}</p>
@@ -16,7 +16,7 @@
   </article>
 </template>
 <script>
-import Hyperlink from '../../domain/Hyperlink'
+import Hyperlink from "../../domain/Hyperlink";
 export default {
   name: "JnInfoCard",
   props: {
@@ -57,11 +57,12 @@ export default {
   background: #f6f5f6;
   border: 1px solid #f1f1f1;
   box-shadow: 1px 3px 4px rgba(0, 0, 0, 0.12);
-  padding: 0.5rem;
+  padding: 0.8rem;
 }
 .info-wrapper > div {
   display: grid;
   grid-template-columns: 50px auto;
+  gap:0.3em;
 }
 .info-header {
   font-size: 1.125rem;
@@ -74,5 +75,9 @@ section {
 }
 .end {
   justify-self: end;
+}
+header {
+  display: grid;
+  grid-template-columns: 9fr 1fr;
 }
 </style>
