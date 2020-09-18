5971abc151da43617b2f5e771377e4d28468b924
Emmy <emmy@jobbnorge.no>
2020-09-18 08:31:31 +0200

    fix: Endret litt på styleguiden

--- styleguide.config.js
+++ styleguide.config.js
@@ -15,21 +15,32 @@ module.exports = {
     title: "Documentation for ui-components",
     theme: {
         color:{
-            base: '#333',
-            light: '#767676',
+            base: '#378663', //Tittel, tabelltitler
+            light: '#72616C', // filsti, Props, Methods..., VIEW CODE, Filter by name
             lightest: '#ccc',
-            link: '#1673b1',
-            linkHover: '#e90',
+            link: '#393036',
+            linkHover: '#D41472',
             focus: 'rgba(22, 115, 177, 0.25)',
-            border: '#e8e8e8',
+            border: '#C4EAD2',
             name: '#690',
             type: '#905',
             error: '#c00',
             baseBackground: '#fff',
             codeBackground: '#f5f5f5',
-            sidebarBackground: '#f5f5f5',
+            sidebarBackground: '#C4EAD2',
             ribbonBackground: '#e90',
-            ribbonText: '#fff'
+            ribbonText: '#fff',
+            codeBase: '#333',
+            codeComment: '#6d6d6d',
+            codePunctuation: '#999',
+            codeProperty: '#905',
+            codeDeleted: '#905',
+            codeString: '#690',
+            codeInserted: '#690',
+            codeOperator: '#9a6e3a',
+            codeKeyword: '#1673b1',
+            codeFunction: '#DD4A68',
+            codeVariable: '#e90',
         }
     }
    
