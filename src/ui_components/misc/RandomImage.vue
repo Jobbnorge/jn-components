226aeff66bccaf2497e02c1937fa3eb91251907d
Vilje Bech <vilje@jobbnorge.no>
2021-03-01 10:58:48 +0100

    fix: Added ImageCircle / RandomImage components

---
+++ src/ui_components/misc/RandomImage.vue
@@ -0,0 +1,22 @@
+<template>
+  <ImageCircle :src="imageSrc" :size="size"></ImageCircle>
+</template>
+
+<script>
+import ImageCircle from "./ImageCircle"
+export default {
+  components: {
+    ImageCircle
+  },
+  props: {
+    images: Array,
+    size: Number 
+  },
+  computed: {
+    imageSrc: function() {
+      let img = Math.floor(Math.random() * (this.images.length-1)) + 1
+      return this.images[img]; 
+    }
+  }
+}
+</script>
