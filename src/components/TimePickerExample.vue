8333e30b1fda64534ce97dc4122f6407564446fd
Vilje Bech <viljebech@gmail.com>
2021-05-12 12:25:12 +0200

    TimePicker OK-button

---
+++ src/components/TimePickerExample.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <JnTimePicker
+      id="time-test"
+      label="Velg klokkeslett"
+      hourLabel="Time med lang tekst"
+      minuteLabel="Minutter"
+      showButton
+      @time-updated="showTime"
+    />
+    <p style="font-size: 1rem; margin-top: 0.5rem;">
+      Tiden er:
+      <span>{{time}}</span>
+    </p>
+  </div>
+</template>
+<script>
+import JnTimePicker from "../ui_components/datetime/JnTimePicker";
+export default {
+  name: "TimePickerExample",
+  components: {
+    JnTimePicker,
+  },
+  data() {
+    return {
+      time: "Ikke satt",
+    };
+  },
+  methods: {
+    showTime: function(e) {
+      this.time = e
+    }
+  }
+};
+</script>
